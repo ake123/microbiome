9cb7511642ac57c64aaf2211c31cf0eb5b46f6a1
raivo-otus <hindstrom.rasmus@gmail.com>
2025-08-19 13:06:55 +0300

    tabs to spaces

--- R/transform.R
+++ R/transform.R
@@ -3,7 +3,7 @@
 #' @param x \code{\link{phyloseq-class}} object
 #' @param transform Transformation to apply. The options include:
 #' 'compositional' (ie relative abundance), 'Z', 'log10', 'log10p',
-#' 'hellinger', 'identity', 'clr', 'alr', or any method from the
+#' 'hellinger', 'identity', 'clr', 'alr', 'rclr' or any method from the
 #' vegan::decostand function.
 #' @param target Apply the transform for 'sample' or 'OTU'.
 #' Does not affect the log transform.
@@ -184,10 +184,9 @@ transform <- function(x, transform = "identity", target = "OTU",
         xt <- scale * x
 
     } else if (transform == "rclr") {
-	
-	xt <- decostand(x, method = "rclr", MARGIN = 2)
-	dimnames(xt) <- dimnames(x)
-	
+        
+        xt <- decostand(x, method = "rclr", MARGIN = 2)
+        dimnames(xt) <- dimnames(x)
         
     } else {
         
